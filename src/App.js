91778aabf8487ef478cac43c5c488918a8b025ea
Carl Olson <colson2275@gmail.com>
2022-03-12 23:09:10 -0500

    add input box and basic filtering

--- src/App.js
+++ src/App.js
@@ -8,25 +8,47 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      filtered: [],
     };
+    console.log('Constructor')
   }
 
   componentDidMount() {
+    console.log('component did mount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response)=> response.json())
     .then((users) => this.setState(()=> {
-      return {monsters: users}
-    },
+      return {monsters: users}  
+      },
+
+    
     ()=>{
       console.log(this.state);
+      this.setState(()=>{return {filtered: users}})
     }
     ))
   }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
-        {this.state.monsters.map((e) => (
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event)=> {
+            console.log(event.target.value);
+            
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(event.target.value.toLowerCase());
+            });
+            this.setState(()=>{
+              return {filtered: filteredMonsters};
+            })
+          }}
+        />
+        {this.state.filtered.map((e) => (
           <div key={e.id}>
             <h1>monster: {e.name}</h1>
           </div>
